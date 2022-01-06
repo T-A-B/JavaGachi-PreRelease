2cdf269e0a7f709fd5a2154da7b02c49d9bee07f
T-A-B <78736596+T-A-B@users.noreply.github.com>
2022-01-06 02:00:45 -0500

    It works

--- javagachiserver/src/main/java/org/javagachi/javagachiserver/JavagachiserverApplication.java
+++ javagachiserver/src/main/java/org/javagachi/javagachiserver/JavagachiserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
+import model.impl.JavaGachi;
 
 @SpringBootApplication
 public class JavagachiserverApplication {
